0f4e8c7d675d555af7e1c10cb5d83998105f0188
palomajurado <palomania@live.com>
2020-10-20 22:10:33 -0500

    Formik validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Form, Button } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      repeatPassword: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Campo obligatorio"),
+      email: Yup.string()
+        .email("El email es obligatorio")
+        .required("Formato incorrecto de email"),
+      password: Yup.string()
+        .required()
+        .oneOf([Yup.ref("repeatPassword")], "Las contraseñas no son iguales"),
+      repeatPassword: Yup.string()
+        .required()
+        .oneOf([Yup.ref("password")], "Las contraseñas no son iguales"),
+    }),
+    onSubmit: (formData) => {
+      console.log(formData);
+    },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      style={{
+        textAlign: "center",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Formulario de registro</h1>
+      <Form style={{ width: "30%" }} onSubmit={formik.handleSubmit}>
+        <Form.Input
+          type="text"
+          placeholder="Nombre y apellidos"
+          name="name"
+          onChange={formik.handleChange}
+          error={formik.errors.name}
+          value={formik.values.name}
+        />
+        <Form.Input
+          type="text"
+          placeholder="Correo electronico"
+          name="email"
+          onChange={formik.handleChange}
+          error={formik.errors.email}
+          value={formik.values.email}
+        />
+        <Form.Input
+          type="password"
+          placeholder="Contraseña"
+          name="password"
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+          value={formik.values.password}
+        />
+        <Form.Input
+          type="password"
+          placeholder="Repetir contraseña"
+          name="repeatPassword"
+          onChange={formik.handleChange}
+          error={formik.errors.repeatPassword}
+          value={formik.values.repeatPassword}
+        />
+        <Button type="submit">Registro</Button>
+        <Button type="button" onClick={formik.handleReset}>
+          Limpiarformulario
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
